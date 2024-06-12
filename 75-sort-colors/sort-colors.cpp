46ae1c2ef93ee5732bc47a4bda9ccea0dc86680a
Gopal Mondal <145310208+I-m-Gopal@users.noreply.github.com>
2024-06-12 22:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (34.14%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n=nums.size(),l=0,r=n-1,id=0;
+        while(id<=r){
+            if(nums[id]==0){
+                swap(nums[l++],nums[id++]);
+            }
+            else if(nums[id]==2){
+                swap(nums[id],nums[r--]);
+            }
+            else{
+                id++;
+            }
+        }
         return;
     }
 };
