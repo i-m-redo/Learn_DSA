a4f19aebb0ed377322eae32887338ab9c6864068
Gopal Mondal <145310208+I-m-Gopal@users.noreply.github.com>
2024-07-05 22:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.82%) - LeetSync

--- 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
+++ 1054-complement-of-base-10-integer/complement-of-base-10-integer.cpp
@@ -3,12 +3,9 @@ public:
     int bitwiseComplement(int n) {
         int m = n;
         int mask = 0;
-
-        if(n==0)
-        return 1;
-        
+        if(n==0) return 1;
         while(m){
-            mask = (mask<<1) | 1;
+            mask  = (mask<<1)|1;
             m = m>>1;
         }
         int ans = (~n) & mask;
