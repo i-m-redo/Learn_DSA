e45cb7e416522bc49816cab27146861d6f4bc45a
Gopal Mondal <145310208+I-m-Gopal@users.noreply.github.com>
2024-07-07 10:34:47 +0530

    Time: 2 ms (53.84%) | Memory: 7.1 MB (30.43%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int full_bottels = numBottles;
+        int empty_bottels = numBottles;
+        int total_empty_bottels = empty_bottels;
+        int drink = full_bottels;
+        
+        while(full_bottels){
+            full_bottels = (total_empty_bottels/numExchange);
+            empty_bottels = (total_empty_bottels%numExchange);
+            total_empty_bottels  = full_bottels + empty_bottels;
+            drink+=full_bottels;
+        }
+        return drink;
+    }
+};
