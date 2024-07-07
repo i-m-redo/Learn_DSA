e9dbd541915d47dea279c2839185780869435738
Gopal Mondal <145310208+I-m-Gopal@users.noreply.github.com>
2024-07-07 15:16:34 +0530

    Time: 60 ms (87.88%) | Memory: 38.6 MB (77.76%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-          sort(nums.begin(),nums.end());
-
-        int minVal = INT_MAX;
-
+        sort(nums.begin(),nums.end());
+        int minValue = INT_MAX;
         int n = nums.size();
+        if(n<=4) return 0;
+        //Convert Right Three
+        minValue = min(minValue,nums[n-4]-nums[0]);
+        //Convert Left three
+        minValue = min(minValue,nums[n-1]-nums[3]);
+        //Convert Left 2 and right 1
+        minValue = min(minValue,nums[n-2]-nums[2]);
+        //Convert Left 1 and right 2
+        minValue = min(minValue,nums[n-3]-nums[1]);
 
-        if(n <= 4) return 0;
-
-        minVal = min(minVal, nums[n-4] - nums[0]);
-        minVal = min(minVal, nums[n-1] - nums[3]);
-        minVal = min(minVal, nums[n-2] - nums[2]);
-        minVal = min(minVal, nums[n-3] - nums[1]);
-
-        return minVal;
+        return minValue;
+        
     }
 };
